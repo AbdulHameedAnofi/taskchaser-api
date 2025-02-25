ab2ee4871da4e004f00f8cb597d9da1df5797cb8
Abdulhameed <anofiolawale@gmail.com>
2025-02-25 07:28:33 +0100

    updated Readme file

--- routes/api.php
+++ routes/api.php
@@ -12,17 +12,17 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('register', [RegisterController::class, 'register']);
-Route::post('login', [LoginController::class, 'login']);
+Route::post('signin', [LoginController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('logout', [LoginController::class, 'logout']);
 
     Route::get('products', [ProductController::class, 'getProducts']);
 
-    Route::post('add-to-cart', [CartController::class, 'addToCart']);
-    Route::post('remove-from-cart/{productId}', [CartController::class, 'removeFromCart']);
+    Route::post('cart', [CartController::class, 'addToCart']);
+    Route::delete('cart/{productId}', [CartController::class, 'removeFromCart']);
     
-    Route::post('checkout-cart', [CartController::class, 'checkoutCart']);
+    Route::post('checkout', [CartController::class, 'checkoutCart']);
 
     Route::post('add-product', [ProductController::class, 'addProduct']);
     Route::get('get-cart', [CartController::class, 'getCart']);
