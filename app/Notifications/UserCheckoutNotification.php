09fafd8babfce1082db8e96c0f5869b4d3be113c
Abdulhameed <anofiolawale@gmail.com>
2025-02-26 09:01:42 +0100

    mail update

--- app/Notifications/UserCheckoutNotification.php
+++ app/Notifications/UserCheckoutNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class UserCheckoutNotification extends Notification implements ShouldQueue
+class UserCheckoutNotification extends Notification
 {
     use Queueable;
 
